5c63167ecfa678d050676dae28313878fa7d9e9d
chanpi23 <channycodez@gmail.com>
2023-09-17 18:23:47 -0400

    updated text

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function openMenu (event,menuName){
     for( let i=0; i<  menuArray.length;) {
         menuArray[i].style.display ='none'
     }
-let tablinks = document.getElementsByClassName ("tablink")
-for (let i=0;i < tablinks.length; i++) {
-    tablinks[i].classList.remove('active-tab')
-}
-document.getElementById(menuName).style.display= 'block'
-event.currentTarget.classList.add('active-tab')
-
-}
-
-document.getElementById("mainLink").click()
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {  // This should be tablinks.length
+       tablinks[i].classList.remove("active-tab");  // Using classList for better readability
+    }
+    
+    document.getElementById(menuName).style.display = "block";
+    event.currentTarget.classList.add("active-tab");  // Using classList for better readability
+  }
+  
+  document.getElementById("mainLink").click(); // Auto-clicks the Pizza tab when the page loads
